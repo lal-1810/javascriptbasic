10e7bd82da0f427faf0c5179d9a4c6c4b4f8e6e2
lal-1810 <146754520+lal-1810@users.noreply.github.com>
2023-10-03 20:20:04 +0000

    Datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict";//treat all js code as newer version
+/* number=>2 to power53
+    bigInt=>
+    string=>"trideb"
+    boolean=>true/false
+    null=>standalone value
+    undefined=>
+    symbol=>unique
+
+
+    
+    ***non primitive***
+    object
+   *** typeof null is an object
+    */
+   console.log(typeof"trideb")
